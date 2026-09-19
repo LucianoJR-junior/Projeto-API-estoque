675f1696c95c4e9a92c9472ec9dc6d907beb0360
ThiagoBachi <thiferbachiega@gmail.com>
2026-09-19 16:52:42 -0300

    Add files via upload

---
+++ ProejtoEstoque/src/main/java/com/example/ProejtoEstoque/service/CategoriasService.java
@@ -0,0 +1,68 @@
+package com.example.ProejtoEstoque.service;
+
+import org.springframework.stereotype.Service;
+import com.example.ProejtoEstoque.repository.CategoriasRepository;
+import com.example.ProejtoEstoque.dto.CategoriasRequestDto;
+import com.example.ProejtoEstoque.dto.CategoriasResponseDto;
+import com.example.ProejtoEstoque.model.Categorias;
+import java.util.List;
+
+@Service
+public class CategoriasService {
+
+    private final CategoriasRepository categoriasRepository;
+
+    public CategoriasService(CategoriasRepository categoriasRepository) {
+        this.categoriasRepository = categoriasRepository;
+    }
+
+    public CategoriasResponseDto criar(CategoriasRequestDto dto) {
+
+        Categorias categoria = new Categorias();
+
+        categoria.setNome(dto.getNome());
+        categoria.setDescricao(dto.getDescricao());
+
+        Categorias categoriaSalva = categoriasRepository.save(categoria);
+
+        return new CategoriasResponseDto(categoriaSalva);
+    }
+
+    public List<CategoriasResponseDto> listarTodos() {
+
+        List<Categorias> categorias = categoriasRepository.findAll();
+
+        return categorias.stream()
+                .map(CategoriasResponseDto::new)
+                .toList();
+    }
+
+    public CategoriasResponseDto buscarPorId(Long id) {
+
+        Categorias categoria = categoriasRepository.findById(id)
+                .orElseThrow();
+
+        return new CategoriasResponseDto(categoria);
+    }
+
+    public CategoriasResponseDto atualizar(Long id, CategoriasRequestDto dto) {
+
+        Categorias categoria = categoriasRepository.findById(id)
+                .orElseThrow();
+
+        categoria.setNome(dto.getNome());
+        categoria.setDescricao(dto.getDescricao());
+
+        Categorias categoriaAtualizada = categoriasRepository.save(categoria);
+
+        return new CategoriasResponseDto(categoriaAtualizada);
+    }
+
+    public void excluir(Long id) {
+
+        Categorias categoria = categoriasRepository.findById(id)
+                .orElseThrow();
+
+        categoriasRepository.delete(categoria);
+    }
+}
