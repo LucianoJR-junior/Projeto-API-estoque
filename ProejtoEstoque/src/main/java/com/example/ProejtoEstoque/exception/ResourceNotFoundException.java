2a0f5a26df8692322caa53462aabf6a6f489ec14
LucianoJR-junior <lucianojr2205@gmail.com>
2026-09-19 18:20:04 -0300

    Delete ProejtoEstoque directory

--- ProejtoEstoque/src/main/java/com/example/ProejtoEstoque/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProejtoEstoque.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-
-}
